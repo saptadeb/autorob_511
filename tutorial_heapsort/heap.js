d6f574e7b61d3b7c2bb8db3bd2bc2f1494334e58
Saptadeep <saptadeb@umich.edu>
2020-09-11 00:54:34 -0400

    heap_sort first commit

--- tutorial_heapsort/heap.js
+++ tutorial_heapsort/heap.js
@@ -27,7 +27,16 @@ minheaper = {};
 
 // define insert function for min binary heap
 function minheap_insert(heap, new_element) {
+    heap.push(new_element)
 
+    if (heap.length > 1) {
+        let current = heap.length - 1
+
+        while (current > 0 && heap[Math.floor((current - 1)/2)] > heap[current]) {
+            [heap[Math.floor((current - 1)/2)], heap[current]] = [heap[current], heap[Math.floor((current - 1)/2)]]
+            current = Math.floor((current - 1)/2)
+        }
+    }
     // STENCIL: implement your min binary heap insert operation
 }
 
@@ -40,16 +49,34 @@ minheaper.insert = minheap_insert;
 
 // define extract function for min binary heap
 function minheap_extract(heap) {
-
+    let top = heap[0]
+    heap[0] = heap[heap.length-1]
+    heap.length--
+    heapify(heap,0)
+    console.log(top)
+    return top
     // STENCIL: implement your min binary heap extract operation
 }
 
 // assign extract function within minheaper object
-
+minheaper.extract = minheap_extract;
     // STENCIL: ensure extract method is within minheaper object
 
-
-
-
-
-
+/* code for recursive extration */
+function heapify(arr, index){
+    largest = index
+    leftchild = 2*index + 1
+    rightchild = 2*index + 2
+    if (leftchild < arr.length && arr[leftchild] < arr[largest]){
+        largest = leftchild
+    }
+    if (rightchild < arr.length && arr[rightchild] < arr[largest]){
+        largest = rightchild
+    }
+    if (largest != index){
+        let temp = arr[index]
+        arr[index] = arr[largest]
+        arr[largest] = temp
+        heapify(arr, largest)
+    }
+}
