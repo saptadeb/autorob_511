13d0f4fa6934375497d370ba0e9c8dd72df0ffc9
Saptadeep <saptadeb@umich.edu>
2020-10-30 17:44:48 -0400

    newbot added

--- robots/robot_newbot.js
+++ robots/robot_newbot.js
@@ -11,13 +11,55 @@ robot = new Object(); // or just {} will create new object
 robot.name = "newbot";
 
 // initialize start pose of robot in the world
-robot.origin = {xyz: [0,0.1,0], rpy:[0,0,0]};  
+robot.origin = {xyz: [0,1,0], rpy:[0,0,0]};  // held a bit over the ground plane
 
 // specify base link of the robot; robot.origin is transform of world to the robot base
-robot.base = "link1";  
+robot.base = "base";  
 
+        
 // specify and create data objects for the links of the robot
-robot.links = {"link1": {}, "link2": {}, "link3": {}, "link4": {} };
+robot.links = {
+    "base": {},  
+    "leg1_upper": {}, 
+    "leg1_middle": {}, 
+    "leg1_lower": {}, 
+    "leg2_upper": {}, 
+    "leg2_middle": {}, 
+    "leg2_lower": {}, 
+    "leg3_upper": {}, 
+    "leg3_middle": {}, 
+    "leg3_lower": {}, 
+    "leg4_upper": {}, 
+    "leg4_middle": {}, 
+    "leg4_lower": {}, 
+    // "leg5_upper": {}, 
+    // "leg5_middle": {}, 
+    // "leg5_lower": {}, 
+    "leg6_upper": {}, 
+    "leg6_middle": {}, 
+    "leg6_lower": {}, 
+    // "leg7_upper": {}, 
+    // "leg7_middle": {}, 
+    // "leg7_lower": {}, 
+    "leg8_upper": {}, 
+    "leg8_middle": {}, 
+    "leg8_lower": {},
+    "clavicle_right": {}, 
+    "clavicle_left": {} , 
+    "shoulder_right": {}, 
+    "shoulder_left": {}, 
+    "upperarm_right": {}, 
+    "forearm_right": {}  
+};
+
+// specify name of endeffector frame
+// robot.endeffector = {};
+// robot.endeffector.frame = "leg1_ankle";
+// robot.endeffector.position = [[0],[0],[0.9],[1]]
+
+robot.endeffector = {};
+robot.endeffector.frame = "forearm_right_yaw";
+robot.endeffector.position = [[0],[0],[0.5],[1]]
 
 //////////////////////////////////////////////////
 /////     DEFINE JOINTS AND KINEMATIC HIERARCHY
@@ -39,27 +81,103 @@ robot.links = {"link1": {}, "link2": {}, "link3": {}, "link4": {} };
 // specify and create data objects for the joints of the robot
 robot.joints = {};
 
-robot.joints.joint1 = {parent:"link1", child:"link2"};
-robot.joints.joint1.origin = {xyz: [0.5,0.3,0.0], rpy:[0,0,0]};
-robot.joints.joint1.axis = [-1.0,0.0,0];  // simpler axis 
+robot.joints.leg1_hip = {parent:"base", child:"leg1_upper"};
+robot.joints.leg1_hip.origin = {xyz: [0.3,0.0,0.9], rpy:[0,Math.PI/2,0]};
+robot.joints.leg1_hip.axis = [0.0,1.0,0.0]; 
 
-robot.joints.joint2 = {parent:"link1", child:"link3"};
-//robot.joints.joint2.origin = {xyz: [-0.2,0.5,0], rpy:[0,0,1.57]};
-robot.joints.joint2.origin = {xyz: [-0.2,0.5,0], rpy:[0,0,Math.PI/2]};
-//robot.joints.joint2.axis = [-0.707,0.707,0];
-robot.joints.joint2.axis = [-Math.cos(Math.PI/4),Math.cos(Math.PI/4),0];
+robot.joints.leg1_knee = {parent:"leg1_upper", child:"leg1_middle"};
+robot.joints.leg1_knee.origin = {xyz: [0.0,0.0,0.4], rpy:[-Math.PI/4,0,0]};
+robot.joints.leg1_knee.axis = [1.0,0.0,0.0]; 
 
-robot.joints.joint3 = {parent:"link3", child:"link4"};
-//robot.joints.joint3.origin = {xyz: [0.5,0,0], rpy:[0,0,-1.57]};
-robot.joints.joint3.origin = {xyz: [0.5,0,0], rpy:[0,0,-Math.PI/2]};
-//robot.joints.joint3.axis = [0.707,-0.707,0];
-robot.joints.joint3.axis = [Math.cos(Math.PI/4),-Math.cos(Math.PI/4),0];
+robot.joints.leg1_ankle = {parent:"leg1_middle", child:"leg1_lower"};
+robot.joints.leg1_ankle.origin = {xyz: [0.0,0.0,0.6], rpy:[Math.PI/2,0,0]};
+robot.joints.leg1_ankle.axis = [1.0,0.0,0.0]; 
+
+robot.joints.leg2_hip = {parent:"base", child:"leg2_upper"};
+robot.joints.leg2_hip.origin = {xyz: [0.3,0.0,-0.9], rpy:[0,Math.PI/2,0]};
+robot.joints.leg2_hip.axis = [0.0,1.0,0.0]; 
+
+robot.joints.leg2_knee = {parent:"leg2_upper", child:"leg2_middle"};
+robot.joints.leg2_knee.origin = {xyz: [0.0,0.0,0.4], rpy:[-Math.PI/4,0,0]};
+robot.joints.leg2_knee.axis = [1.0,0.0,0.0]; 
+
+robot.joints.leg2_ankle = {parent:"leg2_middle", child:"leg2_lower"};
+robot.joints.leg2_ankle.origin = {xyz: [0.0,0.0,0.6], rpy:[Math.PI/2,0,0]};
+robot.joints.leg2_ankle.axis = [1.0,0.0,0.0]; 
+
+robot.joints.leg3_hip = {parent:"base", child:"leg3_upper"};
+robot.joints.leg3_hip.origin = {xyz: [-0.3,0.0,0.9], rpy:[0,-Math.PI/2,0]};
+robot.joints.leg3_hip.axis = [0.0,1.0,0.0]; 
+
+robot.joints.leg3_knee = {parent:"leg3_upper", child:"leg3_middle"};
+robot.joints.leg3_knee.origin = {xyz: [0.0,0.0,0.4], rpy:[-Math.PI/4,0,0]};
+robot.joints.leg3_knee.axis = [1.0,0.0,0.0]; 
+
+robot.joints.leg3_ankle = {parent:"leg3_middle", child:"leg3_lower"};
+robot.joints.leg3_ankle.origin = {xyz: [0.0,0.0,0.6], rpy:[Math.PI/2,0,0]};
+robot.joints.leg3_ankle.axis = [1.0,0.0,0.0]; 
+
+robot.joints.leg4_hip = {parent:"base", child:"leg4_upper"};
+robot.joints.leg4_hip.origin = {xyz: [-0.3,0.0,-0.9], rpy:[0,-Math.PI/2,0]};
+robot.joints.leg4_hip.axis = [0.0,1.0,0.0]; 
+
+robot.joints.leg4_knee = {parent:"leg4_upper", child:"leg4_middle"};
+robot.joints.leg4_knee.origin = {xyz: [0.0,0.0,0.4], rpy:[-Math.PI/4,0,0]};
+robot.joints.leg4_knee.axis = [1.0,0.0,0.0]; 
+
+robot.joints.leg4_ankle = {parent:"leg4_middle", child:"leg4_lower"};
+robot.joints.leg4_ankle.origin = {xyz: [0.0,0.0,0.6], rpy:[Math.PI/2,0,0]};
+robot.joints.leg4_ankle.axis = [1.0,0.0,0.0];  
+
+robot.joints.leg6_hip = {parent:"base", child:"leg6_upper"};
+robot.joints.leg6_hip.origin = {xyz: [0.3,0.0,0], rpy:[0,Math.PI/2,0]};
+robot.joints.leg6_hip.axis = [0.0,1.0,0.0]; 
+
+robot.joints.leg6_knee = {parent:"leg6_upper", child:"leg6_middle"};
+robot.joints.leg6_knee.origin = {xyz: [0.0,0.0,0.4], rpy:[-Math.PI/4,0,0]};
+robot.joints.leg6_knee.axis = [1.0,0.0,0.0]; 
+
+robot.joints.leg6_ankle = {parent:"leg6_middle", child:"leg6_lower"};
+robot.joints.leg6_ankle.origin = {xyz: [0.0,0.0,0.6], rpy:[Math.PI/2,0,0]};
+robot.joints.leg6_ankle.axis = [1.0,0.0,0.0];  
+
+robot.joints.leg8_hip = {parent:"base", child:"leg8_upper"};
+robot.joints.leg8_hip.origin = {xyz: [-0.3,0.0,0.0], rpy:[0,-Math.PI/2,0]};
+robot.joints.leg8_hip.axis = [0.0,1.0,0.0]; 
+
+robot.joints.leg8_knee = {parent:"leg8_upper", child:"leg8_middle"};
+robot.joints.leg8_knee.origin = {xyz: [0.0,0.0,0.4], rpy:[-Math.PI/4,0,0]};
+robot.joints.leg8_knee.axis = [1.0,0.0,0.0]; 
+
+robot.joints.leg8_ankle = {parent:"leg8_middle", child:"leg8_lower"};
+robot.joints.leg8_ankle.origin = {xyz: [0.0,0.0,0.6], rpy:[Math.PI/2,0,0]};
+robot.joints.leg8_ankle.axis = [1.0,0.0,0.0]; 
 
 
-// specify name of endeffector frame
-robot.endeffector = {};
-robot.endeffector.frame = "joint3";
-robot.endeffector.position = [[0.5],[0],[0],[1]]
+robot.joints.clavicle_right_yaw = {parent:"base", child:"clavicle_right"};
+robot.joints.clavicle_right_yaw.origin = {xyz: [0.0,0.0,0.9], rpy:[-Math.PI/2,0,0]};
+robot.joints.clavicle_right_yaw.axis = [0.0,0.0,-1.0]; 
+
+robot.joints.shoulder_right_yaw = {parent:"clavicle_right", child:"shoulder_right"};
+robot.joints.shoulder_right_yaw.origin = {xyz: [0.0,-0.15,1.15], rpy:[Math.PI/2,0,0]};
+robot.joints.shoulder_right_yaw.axis = [0.0,0.707,0.707]; 
+
+robot.joints.upperarm_right_pitch = {parent:"shoulder_right", child:"upperarm_right"};
+robot.joints.upperarm_right_pitch.origin = {xyz: [0.0,0.03,0.80], rpy:[-Math.PI/4,0,0]};
+robot.joints.upperarm_right_pitch.axis = [0.0,1.0,0.0]; 
+
+robot.joints.forearm_right_yaw = {parent:"upperarm_right", child:"forearm_right"};
+robot.joints.forearm_right_yaw.origin = {xyz: [0.0,0.0,0.7], rpy:[0,0,0]};
+robot.joints.forearm_right_yaw.axis = [1.0,0.0,0.0]; 
+
+robot.joints.clavicle_left_roll = {parent:"base", child:"clavicle_left"};
+robot.joints.clavicle_left_roll.origin = {xyz: [0.0,0.0,-0.9], rpy:[-Math.PI/2,0,0]};
+robot.joints.clavicle_left_roll.axis = [0.0,0.0,1.0]; 
+
+robot.joints.shoulder_left_yaw = {parent:"clavicle_left", child:"shoulder_left"};
+robot.joints.shoulder_left_yaw.origin = {xyz: [0.0,0.15,1.15], rpy:[-Math.PI/2,0,0]};
+robot.joints.shoulder_left_yaw.axis = [0.0,0.707,0.707]; 
+
 
 //////////////////////////////////////////////////
 /////     DEFINE LINK threejs GEOMETRIES
@@ -80,15 +198,100 @@ robot.endeffector.position = [[0.5],[0],[0],[1]]
 // define threejs geometries and associate with robot links 
 links_geom = {};
 
-links_geom["link1"] = new THREE.CubeGeometry( 0.7+0.2, 0.5+0.2, 0.2 );
-links_geom["link1"].applyMatrix( new THREE.Matrix4().makeTranslation((0.5-0.2)/2, 0.5/2, 0) );
+links_geom["base"] = new THREE.CubeGeometry( 1, 0.4, 2.3 );
+links_geom["base"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0) );
 
-links_geom["link2"] = new THREE.CubeGeometry( 0.5+0.2, 0.2, 0.2 );
-links_geom["link2"].applyMatrix( new THREE.Matrix4().makeTranslation(0.5/2, 0, 0) );
+links_geom["leg1_upper"] = new THREE.CubeGeometry( 0.3, 0.3, 0.3 );
+links_geom["leg1_upper"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.15) );
 
-links_geom["link3"] = new THREE.CubeGeometry( 0.5+0.2, 0.2, 0.2 );
-links_geom["link3"].applyMatrix( new THREE.Matrix4().makeTranslation(0.5/2, 0, 0) );
+links_geom["leg1_middle"] = new THREE.CubeGeometry( 0.3, 0.3, 0.6 );
+links_geom["leg1_middle"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.3) );
 
-links_geom["link4"] = new THREE.CubeGeometry( 0.5+0.2, 0.2, 0.2 );
-links_geom["link4"].applyMatrix( new THREE.Matrix4().makeTranslation(0.5/2, 0, 0) );
+links_geom["leg1_lower"] = new THREE.CubeGeometry( 0.3, 0.3, 1 );
+links_geom["leg1_lower"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.5) );
+
+links_geom["leg2_upper"] = new THREE.CubeGeometry( 0.3, 0.3, 0.3 );
+links_geom["leg2_upper"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.15) );
+
+links_geom["leg2_middle"] = new THREE.CubeGeometry( 0.3, 0.3, 0.6 );
+links_geom["leg2_middle"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.3) );
+
+links_geom["leg2_lower"] = new THREE.CubeGeometry( 0.3, 0.3, 1 );
+links_geom["leg2_lower"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.5) );
+
+links_geom["leg3_upper"] = new THREE.CubeGeometry( 0.3, 0.3, 0.3 );
+links_geom["leg3_upper"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.15) );
+
+links_geom["leg3_middle"] = new THREE.CubeGeometry( 0.3, 0.3, 0.6 );
+links_geom["leg3_middle"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.3) );
+
+links_geom["leg3_lower"] = new THREE.CubeGeometry( 0.3, 0.3, 1 );
+links_geom["leg3_lower"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.5) );
+
+links_geom["leg4_upper"] = new THREE.CubeGeometry( 0.3, 0.3, 0.3 );
+links_geom["leg4_upper"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.15) );
+
+links_geom["leg4_middle"] = new THREE.CubeGeometry( 0.3, 0.3, 0.6 );
+links_geom["leg4_middle"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.3) );
+
+links_geom["leg4_lower"] = new THREE.CubeGeometry( 0.3, 0.3, 1 );
+links_geom["leg4_lower"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.5) );
+
+// links_geom["leg5_upper"] = new THREE.CubeGeometry( 0.3, 0.3, 0.3 );
+// links_geom["leg5_upper"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.15) );
+
+// links_geom["leg5_middle"] = new THREE.CubeGeometry( 0.3, 0.3, 0.6 );
+// links_geom["leg5_middle"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.3) );
+
+// links_geom["leg5_lower"] = new THREE.CubeGeometry( 0.3, 0.3, 1 );
+// links_geom["leg5_lower"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.5) );
+
+links_geom["leg6_upper"] = new THREE.CubeGeometry( 0.3, 0.3, 0.3 );
+links_geom["leg6_upper"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.15) );
+
+links_geom["leg6_middle"] = new THREE.CubeGeometry( 0.3, 0.3, 0.6 );
+links_geom["leg6_middle"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.3) );
+
+links_geom["leg6_lower"] = new THREE.CubeGeometry( 0.3, 0.3, 1 );
+links_geom["leg6_lower"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.5) );
+
+// links_geom["leg7_upper"] = new THREE.CubeGeometry( 0.3, 0.3, 0.3 );
+// links_geom["leg7_upper"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.15) );
+
+// links_geom["leg7_middle"] = new THREE.CubeGeometry( 0.3, 0.3, 0.6 );
+// links_geom["leg7_middle"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.3) );
+
+// links_geom["leg7_lower"] = new THREE.CubeGeometry( 0.3, 0.3, 1 );
+// links_geom["leg7_lower"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.5) );
+
+links_geom["leg8_upper"] = new THREE.CubeGeometry( 0.3, 0.3, 0.3 );
+links_geom["leg8_upper"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.15) );
+
+links_geom["leg8_middle"] = new THREE.CubeGeometry( 0.3, 0.3, 0.6 );
+links_geom["leg8_middle"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.3) );
+
+links_geom["leg8_lower"] = new THREE.CubeGeometry( 0.3, 0.3, 1 );
+links_geom["leg8_lower"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.5) );
+
+// links_geom["base"] = new THREE.CubeGeometry( 1, 0.4, 1 );
+// links_geom["base"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0.2, 0) );
+
+links_geom["clavicle_right"] = new THREE.CubeGeometry( 0.3, 0.3, 1 );
+links_geom["clavicle_right"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.5) );
+
+links_geom["clavicle_left"] = new THREE.CubeGeometry( 0.3, 0.3, 1 );
+links_geom["clavicle_left"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.5) );
+
+links_geom["shoulder_right"] = new THREE.CubeGeometry( 0.3, 0.3, 0.7 );
+links_geom["shoulder_right"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.35) );
+
+links_geom["upperarm_right"] = new THREE.CubeGeometry( 0.3, 0.3, 0.7 );
+links_geom["upperarm_right"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.35) );
+
+links_geom["forearm_right"] = new THREE.CubeGeometry( 0.3, 0.3, 0.5 );
+links_geom["forearm_right"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.25) );
+
+links_geom["shoulder_left"] = new THREE.CubeGeometry( 0.3, 0.3, 0.7 );
+links_geom["shoulder_left"].applyMatrix( new THREE.Matrix4().makeTranslation(0, 0, 0.35) );
+
 
