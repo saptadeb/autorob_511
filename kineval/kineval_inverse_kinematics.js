3975b46b869b7edecda8a79b484825e01b6c6fe8
Saptadeep <saptadeb@umich.edu>
2020-11-10 18:59:53 -0500

    initial draft

--- kineval/kineval_inverse_kinematics.js
+++ kineval/kineval_inverse_kinematics.js
@@ -38,6 +38,23 @@ kineval.randomizeIKtrial = function randomIKtrial () {
     kineval.params.trial_ik_random.time = cur_time.getTime()-kineval.params.trial_ik_random.start.getTime();
 
     // STENCIL: see instructor for random time trial code
+    endeffector_world = matrix_multiply(robot.joints[robot.endeffector.frame].xform,robot.endeffector.position);
+
+    // compute distance of endeffector to target
+    kineval.params.trial_ik_random.distance_current = Math.sqrt(
+            Math.pow(kineval.params.ik_target.position[0][0]-endeffector_world[0][0],2.0)
+            + Math.pow(kineval.params.ik_target.position[1][0]-endeffector_world[1][0],2.0)
+            + Math.pow(kineval.params.ik_target.position[2][0]-endeffector_world[2][0],2.0) );
+
+    // if target reached, increment scoring and generate new target location
+    // KE 2 : convert hardcoded constants into proper parameters
+    if (kineval.params.trial_ik_random.distance_current < 0.01) {
+        kineval.params.ik_target.position[0][0] = 1.2*(Math.random()-0.5);
+        kineval.params.ik_target.position[1][0] = 1.2*(Math.random()-0.5)+1.5;
+        kineval.params.ik_target.position[2][0] = 0.7*(Math.random()-0.5)+0.5;
+        kineval.params.trial_ik_random.targets += 1;
+        textbar.innerHTML = "IK trial Random: target " + kineval.params.trial_ik_random.targets + " reached at time " + kineval.params.trial_ik_random.time;
+    }
 }
 
 kineval.iterateIK = function iterate_inverse_kinematics(endeffector_target_world, endeffector_joint, endeffector_position_local) {
@@ -57,8 +74,85 @@ kineval.iterateIK = function iterate_inverse_kinematics(endeffector_target_world
     // dtheta = alpha * robot.dq   // alpha: step length
 
 
+    var endeXYZWorld = matrix_multiply(robot.joints[endeffector_joint].xform, endeffector_position_local);
+    var endeRPYWorld = xformTransTheta(robot.joints[endeffector_joint].xform);
+    var endeVec = [];
+    var i;
+    for (i = 0; i< 3; i++){
+        endeVec[i] = [endeXYZWorld[i][0]];
+        endeVec[i+3] = [endeRPYWorld[i][0]];
+    }
+    robot.dx = [];
+    for (i = 0; i< 3; i++){
+        robot.dx[i] = [endeffector_target_world.position[i] - endeVec[i][0]];
+    }
+    for (i = 3; i< 6; i++){
+        if (kineval.params.ik_orientation_included){
+            robot.dx[i] = [endeffector_target_world.orientation[i-3] - endeVec[i][0]];
+        }
+        else{
+            robot.dx[i] = [0];
+        }
+    }
+
+
+    robot.jacobian = [];
+    var indexJoint = 0;
+    var curJoint = endeffector_joint;
+    
+
+    
+    var flagWhile = true;
+    while (flagWhile){
+        var tempAxis = matrix_vec_multiply(robot.joints[curJoint].xform,robot.joints[curJoint].axis)
+         tempAxis = vector_normalize(tempAxis);
+         if (robot.joints[curJoint].type === "prismatic"){
+             robot.jacobian[indexJoint] = [tempAxis[0], tempAxis[1], tempAxis[2], 0, 0, 0];
+         }else{
+             var jointXYZWorld = matrix_multiply(robot.joints[curJoint].xform, [[0],[0],[0],[1]]);
+             var diffJointXYZ = vec_minus(endeXYZWorld,jointXYZWorld);
+             var tempC = vector_cross(tempAxis, diffJointXYZ);
+             robot.jacobian[indexJoint] = [tempC[0], tempC[1], tempC[2], tempAxis[0], tempAxis[1], tempAxis[2]];
+         }
+         if (robot.joints[curJoint].parent === robot.base){
+             flagWhile = false;
+         }
+         indexJoint++;
+         curJoint = robot.links[robot.joints[curJoint].parent].parent;
+     }
+     robot.dq = [];
+     if (!kineval.params.ik_pseudoinverse){
+         robot.dq = matrix_multiply(robot.jacobian, robot.dx);
+     }
+     else{
+         var jacobT = matrix_transpose(robot.jacobian);
+         robot.dq = matrix_multiply(matrix_pseudoinverse(jacobT), robot.dx);
+     
+     }
+     flagWhile = true;
+     indexJoint = 0;
+     curJoint = endeffector_joint;
+     while (flagWhile){
+         robot.joints[curJoint].control += kineval.params.ik_steplength*robot.dq[indexJoint][0];
+         indexJoint++;
+         if (robot.joints[curJoint].parent === robot.base){
+             flagWhile = false;
+         }
+         curJoint = robot.links[robot.joints[curJoint].parent].parent;
+     }
 
 }
 
+function xformTransTheta(xform){
+    
+    var theta1=[Math.atan2( xform[2][1], xform[2][2])];
+    var theta3=[Math.atan2( xform[1][0], xform[0][0])];
+    var temp = Math.pow(xform[2][1], 2)+ Math.pow(xform[2][2], 2);
+    temp = Math.pow(temp,0.5);
+    var theta2=[Math.atan2(-xform[2][0],temp)];
+    
+    var theta = [theta1,theta2,theta3];
+    return theta;
+}
 
 
